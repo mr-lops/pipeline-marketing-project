8fc92bd50c8dd03a9c237639a1668c46845cc8fc
Vitor S. Lopes <vitorsantoslopes1@gmail.com>
2023-05-08 22:56:13 -0300

    chore:codigo sql que cria o banco

--- dados/criar_OLAP_database.sql
+++ dados/criar_OLAP_database.sql
@@ -1,3 +1,9 @@
+-- cria banco dados
+CREATE DATABASE loan;
+
+-- Conecte ao banco de dados criado e execute as instruções SQL abaixo
+
+-- Cria tabela client
 CREATE TABLE client
 (
     id SERIAL PRIMARY KEY,
@@ -10,6 +16,7 @@ CREATE TABLE client
     loan BOOLEAN
 );
 
+-- Cria tabela campaign
 CREATE TABLE campaign
 (
     id SERIAL PRIMARY KEY,
@@ -23,7 +30,7 @@ CREATE TABLE campaign
     last_contact_date DATE    
 );
 
-
+-- Cria tabela economics
 CREATE TABLE economics
 (
     id SERIAL PRIMARY KEY,
@@ -34,8 +41,29 @@ CREATE TABLE economics
     number_employed FLOAT
 );
 
+-- Cria o relacionamento entre as tabelas
 ALTER TABLE campaign
 ADD CONSTRAINT fk_campaign_client FOREIGN KEY (client_id) REFERENCES client(id);
 
 ALTER TABLE economics
 ADD CONSTRAINT fk_economics_client FOREIGN KEY (client_id) REFERENCES client(id);
+
+-- Cria usuario airflow
+CREATE USER airflow WITH
+PASSWORD 'MyP@ssword';
+
+-- Da Permissão ao usuario airflow de somente inserir registros
+GRANT INSERT ON economics,campaign,client TO airflow;
+
+-- Cria grupo data_analyst
+CREATE ROLE data_analyst;
+
+-- Da Permissão ao grupo data_analyst de somente consultar registros
+GRANT SELECT ON economics,campaign,client TO data_analyst;
+
+-- Criar usuario joao
+CREATE USER joao WITH
+PASSWORD 'MinhaSenha';
+
+-- Adiciona o usuario joao ao grupo data_analyst
+GRANT data_analyst TO joao;
