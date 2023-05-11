aeb788f9266eddceb7f484b41c599589619726a2
Vitor S. Lopes <vitorsantoslopes1@gmail.com>
2023-05-11 20:45:57 -0300

    CHORE:criando dag para airflow

--- airflow-dag/airflow_etl.py
+++ airflow-dag/airflow_etl.py
@@ -2,7 +2,44 @@ import pandas as pd
 import numpy as np
 from dotenv import load_dotenv
 import os
+from airflow import DAG
+import airflow.utils.dates as airflow_date
+from airflow.operators.python import PythonOperator
+from airflow.operators.email import EmailOperator
 
 load_dotenv()
 
-os.environ.get('DB_NAME')
+#os.environ.get('DB_NAME')
+
+default_agrs = {
+    'description' : 'Realiza a extração, tranformação e carga de dados de um bucket S3 para um banco de dados Postgres',
+    'start_date' : airflow_date.days_ago(15),
+}
+
+with DAG (
+    dag_id="Pipeline ETL Marketing",
+    default_args= default_agrs,
+    schedule_interval= None,
+    tags=['Marketing','ETL'],
+    
+    
+) as dag:
+    
+    extract = PythonOperator(
+        task_id = 'extract_task'
+    )
+    
+    transform = PythonOperator(
+        task_id = 'tansform_task'
+    )
+    
+    load = PythonOperator(
+        task_id = 'load_task'
+    )
+    
+    notify = EmailOperator(
+        task_id = 'notify_task'
+    )
+    
+    extract >> transform >> load >> notify
+    
