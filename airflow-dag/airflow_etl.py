07dcba0c6f49b7adb42e9d6b14de1b65003d5dbf
Vitor S. Lopes <vitorsantoslopes1@gmail.com>
2023-05-12 22:39:32 -0300

    CHORE:dag para airflow

--- airflow-dag/airflow_etl.py
+++ airflow-dag/airflow_etl.py
@@ -1,5 +1,9 @@
+# Importa pacotes que serão utilizados
 import pandas as pd
 import numpy as np
+import psycopg2
+import datetime
+import pytz
 from dotenv import load_dotenv
 import os
 from airflow import DAG
@@ -7,18 +11,35 @@ import airflow.utils.dates as airflow_date
 from airflow.operators.python import PythonOperator
 from airflow.operators.email import EmailOperator
 
+# Carrega as variaveis de ambiente definidas no arquivo ".env"
 load_dotenv()
 
-#os.environ.get('DB_NAME')
+# Função de extração dos dados do bucket S3
+def _extract():
+    pass
 
-default_agrs = {
-    'description' : 'Realiza a extração, tranformação e carga de dados de um bucket S3 para um banco de dados Postgres',
-    'start_date' : airflow_date.days_ago(7),
-}
+# Função de transformação dos dados, os deixando no padrão para serem enviados
+def _transform():
+    pass
+
+# Função de carregamento dos dados ja transformados para o banco Postgres
+def _load():
+    
+    # Conecta com o banco de dados Postgres
+    conn = psycopg2.connect(
+        host=os.environ.get('DB_HOST'),
+        database=os.environ.get('DB_NAME'),
+        user=os.environ.get('DB_USER'),
+        password=os.environ.get('DB_PASSWORD'),
+        port = os.environ.get('DB_PORT'))
+    
+    # Cria um cursos para executar as instruções SQL
+    cur = conn.cursor()
 
 with DAG (
     dag_id="Pipeline ETL Marketing",
-    default_args= default_agrs,
+    description = 'Realiza a extração, tranformação e carga de dados de um bucket S3 para um banco de dados Postgres',
+    start_date = airflow_date.days_ago(7),
     schedule_interval= "0 22 * * 6",
     tags=['Marketing','ETL'],
     
@@ -26,19 +47,33 @@ with DAG (
 ) as dag:
     
     extract = PythonOperator(
-        task_id = 'extract_task'
+        task_id = 'extract_task',
+        python_callable= _extract,
+        email_on_failure= True,
+        email = os.environ.get('EMAIL')
     )
     
     transform = PythonOperator(
-        task_id = 'tansform_task'
+        task_id = 'tansform_task',
+        python_callable= _extract,
+        email_on_failure= True,
+        email = os.environ.get('EMAIL')
     )
     
     load = PythonOperator(
-        task_id = 'load_task'
+        task_id = 'load_task',
+        python_callable= _extract,
+        email_on_failure= True,
+        email = os.environ.get('EMAIL')
     )
     
     notify = EmailOperator(
-        task_id = 'notify_task'
+        task_id = 'notify_task',
+        email_on_failure= True,
+        email = os.environ.get('EMAIL'), 
+        to= os.environ.get('EMAIL'),
+        subject='Pipeline Realizado Com Sucesso',
+        html_content=f"<p> O Pipeline foi executado com sucesso as {str(datetime.datetime.now(pytz.timezone('America/Sao_Paulo')))}. <p>"
     )
     
     extract >> transform >> load >> notify
