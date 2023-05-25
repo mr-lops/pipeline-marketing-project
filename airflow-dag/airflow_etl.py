a6e953279adf10192313a30547b4304b31332433
Vitor S. Lopes <vitorsantoslopes1@gmail.com>
2023-05-25 09:52:54 -0300

    Dag Airflow

--- airflow-dag/airflow_etl.py
+++ airflow-dag/airflow_etl.py
@@ -1,7 +1,4 @@
 # Importa pacotes que serão utilizados
-import pandas as pd
-import numpy as np
-import psycopg2
 from datetime import datetime, timedelta
 from pytz import timezone
 from dotenv import load_dotenv
@@ -10,21 +7,37 @@ from airflow import DAG
 import airflow.utils.dates as airflow_date
 from airflow.operators.python import PythonOperator
 from airflow.operators.email import EmailOperator
+import shutil
+
+# Pastas temporarias onde irão ficar os dados que foram extraidos e os dados que foram tratados.
+staging = "tmp/staging/"
+refined = "tmp/refined/"
+
 
 # Carrega as variaveis de ambiente definidas no arquivo ".env"
 load_dotenv()
 
 # Função de extração dos dados do bucket S3
 def _extract():
-    pass
+    import boto3
+    
+    aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
+    aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+    # Configura as credenciais de acesso à conta da AWS
+    s3 = boto3.resource('s3', aws_access_key_id=aws_access_key_id,
+                        aws_secret_access_key=aws_secret_access_key)
+
 
 # Função de transformação dos dados, os deixando no padrão para serem enviados
 def _transform():
-    pass
+    import pandas as pd
+    import numpy as np
+    
 
 # Função de carregamento dos dados ja transformados para o banco Postgres
 def _load():
-    
+    import psycopg2
     # Conecta com o banco de dados Postgres
     conn = psycopg2.connect(
         host=os.environ.get('DB_HOST'),
@@ -33,8 +46,29 @@ def _load():
         password=os.environ.get('DB_PASSWORD'),
         port = os.environ.get('DB_PORT'))
     
-    # Cria um cursos para executar as instruções SQL
+    # Cria um cursor para executar as instruções SQL
     cur = conn.cursor()
+    
+    # Adiciona os dados dos arquivos csv para o banco de dados
+    cur.execute(f"""
+       \copy client from '{refined}client.csv' DELIMITER ',' CSV HEADER 
+    """)
+    cur.execute(f"""
+       \copy campaign from '{refined}campaign.csv' DELIMITER ',' CSV HEADER 
+    """)
+    cur.execute(f"""
+       \copy economics from '{refined}economics.csv' DELIMITER ',' CSV HEADER
+    """)
+    
+    # Aplica as alterações no banco de dados
+    conn.commit()
+    
+    # Fecha conexão com o banco de dados
+    cur.close()
+    conn.close()
+    
+    # remove pasta temporaria
+    shutil.rmtree("tmp", ignore_errors=False, onerror=None)
 
 with DAG (
     dag_id="Pipeline ETL Marketing",
